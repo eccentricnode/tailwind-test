cf6cc8b36d4873638a4b69da64fcb75a458f22be
eccentricnode <shival.bigman@gmail.com>
2022-04-15 19:37:08 -0700

    created transparent colorful navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,38 @@
 import { MenuIcon } from "@heroicons/react/outline";
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Navbar() {
   const [isActive, setActive] = useState("false");
-
   const handleToggle = () => {
     setActive(!isActive);
-  };    
+  };
+  
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    console.log(position);
+    setScrollPosition(position);
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }, [])
+
+  const isNavbarAtStart = () => {
+   if (scrollPosition >= 35) {
+    return 'text-base !text-white backdrop-filter backdrop-blur-md transition-opacity delay-150 ease-in-out py-1 bg-gray-800 opacity-90 border-b border-gray-200'
+   }
+   else {
+     return 'text-base text-gray-700 backdrop-filter backdrop-blur-lg'
+   }
+  };
 
   return (
-    <header className="fixed w-full transition-all top-0 z-10 shadow-lg backdrop-filter backdrop-blur-lg border-b border-gray-200">
+    <header className={`fixed w-full transition-all top-0 z-10 ${isNavbarAtStart()}`}>
       <nav className="flex flex-wrap justify-center py-4 md:py-0 px-4 text-lg text-gray-700">
         <MenuIcon
           id="menu-button"
@@ -20,7 +43,7 @@ export default function Navbar() {
           stroke="currentColor"
         />
         <div className={`${isActive ? "hidden" : ""} w-full md:flex md:items-center md:w-auto`} id="menu">
-            <ul className="text-base text-gray-700 pt-4 md:flex md:justify-between md:pt-0">
+            <ul className="pt-4 md:flex md:justify-between md:pt-0">
                 <li>
                 <a className="md:p-4 py-2 block hover:text-purple-400" href="#">
                     Features
